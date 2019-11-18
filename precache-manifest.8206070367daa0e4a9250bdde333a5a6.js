36badf0746ea0c7b60432532c992a5683784f7d0
bisha7601 <57886775+bisha7601@users.noreply.github.com>
2019-11-18 07:27:36 +0200

    Add files via upload

---
+++ precache-manifest.8206070367daa0e4a9250bdde333a5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4634c6a362a7606e64e20877cc0e2ee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc29e3eba260795fc6fe",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "69627af942f16428ca07",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bc29e3eba260795fc6fe",
+    "url": "/static/js/2.1ea92aaf.chunk.js"
+  },
+  {
+    "revision": "69627af942f16428ca07",
+    "url": "/static/js/main.43b37c58.chunk.js"
+  },
+  {
+    "revision": "3e4d1c7c963290bcaca5",
+    "url": "/static/js/runtime-main.a94c365d.js"
+  },
+  {
+    "revision": "b6c3245da011c4718f89cbbd223800cd",
+    "url": "/static/media/shopping-cart.b6c3245d.svg"
+  }
+]);
